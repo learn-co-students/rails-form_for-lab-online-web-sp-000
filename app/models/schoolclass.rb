b7979a2148eb25a6afb14cd5d497d7ff7c829440
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-20 10:58:27 +0100

    mark schoolclass inheritance

--- app/models/schoolclass.rb
+++ app/models/schoolclass.rb
@@ -1,3 +1,3 @@
-class SchoolClass
+class SchoolClass < ActiveRecord::Base
   
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Dir[Rails.root.join("app/**/*.rb")].each { |f| require f }
+
+
 # Initialize the Rails application.
 Rails.application.initialize!

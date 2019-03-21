dfb3c5b052baf0b0b3cbf407505374c88adeb0ff
github username <github email address>
2019-03-21 14:05:23 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :students, only: [:index, :show, :new, :create, :edit, :update]
+  patch 'student/:id', to: 'student#update'
+
+  resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
+  patch 'school_class/:id', to: 'school_class#update'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

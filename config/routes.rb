40401a727dac9b6e01a0335b88c1148eaca802b9
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-04-12 15:36:05 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,30 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get "students", to: "students#index"
+
+  get "students/new", to: "students#new", as: :new_student
+
+  post "students", to: "students#create"
+
+  get "students/:id", to: "students#show", as: :student
+
+  get "students/:id/edit", to: "students#edit", as: :edit_student
+
+  patch "students/:id", to: "students#update"
+
+
+  get "school_classes", to: "school_classes#index"
+
+  get "school_classes/new", to: "school_classes#new", as: :new_school_class
+
+  post "school_classes", to: "school_classes#create"
+
+  get "school_classes/:id", to: "school_classes#show", as: :school_class
+
+  get "school_classes/:id/edit", to: "school_classes#edit", as: :edit_school_class
+
+  patch "school_classes/:id", to: "school_classes#update"
+
 end

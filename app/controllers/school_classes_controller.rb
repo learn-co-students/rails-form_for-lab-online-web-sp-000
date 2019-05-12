ba0db13f8601436a7e89ad3c9a5c5f9a1ba99748
Btate712 <btate712@gmail.com>
2019-05-12 08:04:16 -0500

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,11 @@
+class SchoolClassesController < ApplicationController
+  def create
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
+  end
+end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,11 @@
+class StudentsController < ApplicationController
+  def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
+end

6fc3276c7314306def810da1d7adc8174c68d3b8
Yingqi Chen <chanwk18@hotmail.com>
2019-09-15 00:38:13 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,19 +4,27 @@ class SchoolClassesController < ApplicationController
   end
 
   def new
+    @school_class=SchoolClass.new
   end
 
   def create
-    
+    @school_class=SchoolClass.create(some_params(:title,:room_number))
+    redirect_to school_class_path(@school_class)
   end
 
   def edit
+    @school_class=SchoolClass.find(params[:id])
   end
 
   def update
+    @school_class=SchoolClass.find(params[:id])
+    @school_class.update(some_params(:title,:room_number))
+    redirect_to school_class_path(@school_class)
   end
 
   def show
+    @school_class=SchoolClass.find(params[:id])
+  end
 
 private
   def some_params(*args)
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,18 +4,26 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student=Student.new
   end
 
   def create
+    @student=Student.create(some_params(:first_name,:last_name))
+    redirect_to student_path(@student)
   end
 
   def edit
+    @student=Student.find(params[:id])
   end
 
   def update
+    @student=Student.find(params[:id])
+    @student.update(some_params(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
 
   def show
+    @student=Student.find(params[:id])
   end
 
 private

332d66b69a16443505caa2cd0823086e149dca3b
jgrantps <jgrantps@hotmail.com>
2019-10-06 10:42:13 -0300

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,17 +13,34 @@ class StudentsController < ApplicationController
     end
 
     def create
+        student = Student.new(student_params)
+        
+        if student.save
+            redirect_to student_path(student)
+        else
+            redirect_to new_student_path
+        end
     end
 
     def edit
+
+        @student = Student.find(params[:id])
+
     end
 
     def update
+        @student = Student.find(params[:id])
+        @student.update(student_params)
+        redirect_to student_path(@student)
     end
 
 
     def destroy
     end
 
-
+    private
+    def student_params
+        params.require(:student).permit(:first_name, :last_name)
+    end
+    
 end

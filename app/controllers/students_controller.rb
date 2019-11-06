fc250dde2a9be73b8cc43f8e640138502ea182a9
rohanphillips <rohan@solutions-insurance.com>
2019-11-06 10:22:40 -0600

    Student new and show

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,4 +6,24 @@ class StudentsController < ApplicationController
   def new
     @student = Student.new
   end
+
+  def create
+	  @student = Student.new(student_params(:first_name, :last_name))
+	  @student.save
+	  redirect_to student_path(@student)
+	end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  private
+ 
+	def student_params(*args)
+		params.require(:student).permit(*args)
+	end
 end

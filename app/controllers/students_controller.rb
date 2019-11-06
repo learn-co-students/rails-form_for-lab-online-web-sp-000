ea93b68168070c56d940cda7ece511d28df6e679
rohanphillips <rohan@solutions-insurance.com>
2019-11-06 10:25:24 -0600

    Student edit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,6 +11,12 @@ class StudentsController < ApplicationController
 	  @student = Student.new(student_params(:first_name, :last_name))
 	  @student.save
 	  redirect_to student_path(@student)
+  end
+  
+  def update
+	  @student = Student.find(params[:id])
+	  @student.update(student_params(:first_name, :last_name))
+	  redirect_to student_path(@student)
 	end
 
   def show

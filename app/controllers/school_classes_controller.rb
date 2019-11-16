153d911e72b5fec9b5b1288927b728e27f9225f4
Lauren McCoy <lm_lutec@outlook.com>
2019-11-16 10:49:05 -0500

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,5 +1,31 @@
 class SchoolClassesController < ApplicationController
 
+    def new 
+        @schoolclass = SchoolClass.new 
+    end 
 
+    def create
+        @schoolclass = SchoolClass.new
+        @schoolclass.title = params[:school_class][:title]
+        @schoolclass.room_number = params[:school_class][:room_number]
+        @schoolclass.save
+        redirect_to school_class_path(@schoolclass)
+    end 
+
+    def show
+        @schoolclass = SchoolClass.find(params[:id])
+    end 
+
+    def edit
+        @schoolclass = SchoolClass.find(params[:id])
+        redirect_to school_class_path(@schoolclass)
+    end 
+
+
+    private
+
+    def schoolclass_params(*args)
+        params.require(:schoolclass) .permit(*args)
+    end 
 
 end 
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,19 @@
 class StudentsController < ApplicationController
 
+    def new
+        @student = Student.new  
+    end 
 
+
+    def create
+        @student = Student.new
+        @student.first_name = params[:student][:first_name]
+        @student.last_name = params[:student][:last_name]
+        @student.save
+        redirect_to student_path(@student)
+    end 
+
+    def show 
+    end 
 
 end

44daceae24a43279f6d658a6db969ac34b516e3b
zdsmit <zdsmith001@gmail.com>
2020-01-23 14:48:28 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,7 +15,17 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new(post_params(:first_name, :last_name))
     @student.save
-    redirect_to students_path(@student)
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(post_params(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
 
   private

fcba60dfa603008850cc4bcdd326220ed6fa1c54
Chinedu Arinze <c.s.arinze@gmail.com>
2020-01-23 13:46:34 -0500

    Add all necessary code for SchoolClassController, simple update to SchoolClass show page, & SchoolClass form layout

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,16 +1,33 @@
 class SchoolClassesController < ApplicationController
+   layout "school_classes_form", only: [:new, :edit]
+   
     def show
+        @school_class = SchoolClass.find(params[:id])
     end
     
     def new
+        @school_class = SchoolClass.new
     end
 
     def create
+        @school_class =SchoolClass.new(post_params)
+        @school_class.save
+        redirect_to school_class_path(@school_class)
     end
 
     def edit
+        @school_class = SchoolClass.find(params[:id])
     end
 
-    def update
+    def update        
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(post_params)
+        redirect_to school_class_path(@school_class)
+    end
+
+    private
+
+    def post_params
+        params.require(:school_class).permit(:title, :room_number)
     end
 end

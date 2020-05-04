6892fa8908b78614318067eca812ee53c0ab35ca
Anthony Tilelli <Anthony@tilelli.me>
2020-05-03 20:42:24 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,19 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2) do
-  create_table 'school_classes', force: :cascade do |t|
-    t.string   'title',       null: false
-    t.integer  'room_number', null: false
-    t.datetime 'created_at',  null: false
-    t.datetime 'updated_at',  null: false
+
+  create_table "school_classes", force: :cascade do |t|
+    t.string   "title",       null: false
+    t.integer  "room_number", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table 'students', force: :cascade do |t|
-    t.string   'first_name'
-    t.string   'last_name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "students", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end

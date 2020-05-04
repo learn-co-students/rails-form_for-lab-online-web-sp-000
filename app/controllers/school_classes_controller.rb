d2be244b8c6b975c7ef1b5a0ca34ec2734d76acc
Anthony Tilelli <Anthony@tilelli.me>
2020-05-03 23:18:40 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,13 +4,30 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @schoolclass = SchoolClass.create!(params.require(:school_class).permit(:title,:room_number))
+    @schoolclass = SchoolClass.create!(update_or_create)
     redirect_to school_class_path(@schoolclass)
   end
 
-  def show; end
+  def show
+    @schoolclass = find_class
+  end
 
-  def edit; end
+  def edit
+    @schoolclass = find_class
+  end
 
-  def update; end
+  def update
+    @schoolclass = SchoolClass.update(update_or_create)
+    redirect_to school_class_path(@schoolclass)
+  end
+
+  private
+
+  def find_class 
+    SchoolClass.find(params.require(:id))
+  end
+
+  def update_or_create
+    params.require(:school_class).permit(:title,:room_number)
+  end
 end

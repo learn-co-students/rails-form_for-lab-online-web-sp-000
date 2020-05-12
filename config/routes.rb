9472f90af9fabddf58b769d844d643c734793db4
github username <github email address>
2020-05-12 13:19:50 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
+  resources :students, only: [:index, :show, :new, :create, :edit, :update]
+  # delete 'students/:id(.:format)', :to => 'students#destroy'
+  # delete 'school_classes/:id(.:format)', :to => 'school_classes#destroy'
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

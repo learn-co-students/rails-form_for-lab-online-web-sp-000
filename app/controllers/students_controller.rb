520d370e5274c5cb5445aeb690c96826662a6a74
Michael Carroll <carrollm2@gmail.com>
2020-05-24 01:46:09 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,7 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
@@ -18,6 +19,10 @@ class StudentsController < ApplicationController
     @student.update(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
+
+  def edit
+	  @student = Student.find(params[:id])
+	end
 
   private
 

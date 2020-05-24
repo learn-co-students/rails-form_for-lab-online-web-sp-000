a30d8e99d20aadcfecc6e727f4134af0e47c0071
CoderJay06 <jaycru06@yahoo.com>
2020-05-24 01:55:53 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,13 +1,24 @@
 class SchoolClassesController < ApplicationController
    def new 
+      @school_class = SchoolClass.new
    end 
 
    def create 
-      @school_class = SchoolClass.new
-      @school_class.title = params[:school_class][:title]
-      @school_class.room_number = params[:school_class][:room_number]
+      @school_class = SchoolClass.create(
+         params.require(:school_class).permit(:title, :room_number)
+      )
+      # @school_class.title = params[:school_class][:title]
+      # @school_class.room_number = params[:school_class][:room_number]
       @school_class.save 
       redirect_to school_class_path(@school_class)
+   end 
+
+   def show 
+      @school_class = SchoolClass.find(params[:id])
+   end 
+
+   def edit 
+      @school_class = SchoolClass.find(params[:id])
    end 
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,12 @@
 class StudentsController < ApplicationController
+   def new 
+   end 
    
+   def create 
+      @student = Student.new 
+      @student.first_name = params[:student][:first_name]
+      @student.last_name = params[:student][:last_name]
+      @student.save 
+      redirect_to student_path(@student)
+   end 
 end

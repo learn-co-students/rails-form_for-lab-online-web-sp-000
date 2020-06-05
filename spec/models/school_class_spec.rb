4e15bb765ae63a173e40c8b91e8e9f7cba72f02e
zchesny <z.chesny@gmail.com>
2020-06-05 11:21:43 -0700

    Done.

--- spec/models/school_class_spec.rb
+++ spec/models/school_class_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
-describe SchoolClass do
-  before(:each) do
-    @school_class = SchoolClass.create!(title: "Computer Science", room_number: 5)
+RSpec.describe SchoolClass, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe SchoolClass do
+    before(:each) do
+      @school_class = SchoolClass.create!(title: "Software Engineering 110", room_number: 110)
+    end
+  
+    it 'can be created' do
+      expect(@school_class).to be_valid
+    end
+  
   end
-
-  it 'can be created' do
-    expect(@school_class).to be_valid
-  end
-
 end

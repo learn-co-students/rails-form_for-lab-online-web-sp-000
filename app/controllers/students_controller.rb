379687faa3eea56a6eeb0a3272e2e6fa216416b7
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-24 19:10:33 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,37 @@
+
+class StudentsController < ApplicationController
+
+  def show
+    set_student!
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+    set_student!
+  end
+
+  def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def update
+    set_student!
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def set_student!
+    @student = Student.find(params[:id])
+  end
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
+end

06c8cb3a957e701f59ca24e408648027a6b694a7
bruffin1221 <bruffin1221@gmail.com>
2020-07-05 15:24:55 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Muhammad", last_name: "Ali")
+Student.create(first_name: "Mike", last_name: "Tyson")
+Student.create(first_name: "Magic", last_name: "Jonhson")
+Student.create(first_name: "Lebron", last_name: "James")
+Student.create(first_name: "Serena", last_name: "Williams")
+SchoolClass.create(title:"Taking Souls 101", room_number: 1)
+SchoolClass.create(title:"Championship Mindset", room_number: 2)
+SchoolClass.create(title:"Breaking Wills", room_number: 3)
+SchoolClass.create(title:"Greatness", room_number: 4)
+SchoolClass.create(title:"Passion 200", room_number: 5)

342d59c597290b0c9d85227dfc1d43a088bb027a
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-09 19:28:13 -0700

    add new, create, and private method to school_classes_controller.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,16 @@
 class SchoolClassesController < ApplicationController
 
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create 
+    @school_class = SchoolClass.create(school_class_params)
+    redirect_to student_path(@student)
+  end
+
+  private 
+  def school_class_params
+    params.require(:school_class).permit(:title, :room_number)
+  end
 end

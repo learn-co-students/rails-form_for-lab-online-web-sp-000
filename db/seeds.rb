243c2b92b1f4afbfbf0d5f9e5f2912a2d01947ec
mbade1 <mbade314@gmail.com>
2020-07-12 16:26:15 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+SchoolClass.create(:title => "Science", :room_number => 101)
+SchoolClass.create(:title => "English", :room_number => 201)
+SchoolClass.create(:title => "History", :room_number => 301)
+SchoolClass.create(:title => "Math", :room_number => 401)
+
+
+
+Student.create(:first_name => "Nathan", :last_name => "Kemp")
+Student.create(:first_name => "Azriel", :last_name => "Richardson")
+Student.create(:first_name => "Jake", :last_name => "Townsend")

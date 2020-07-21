80decb22e257cefd514a2f238bec8a58f5e2e8b1
Mark A Guza <guzamaguza@gmail.com>
2020-07-21 03:09:56 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,27 @@
+class StudentsController < ApplicationController
+
+    def create
+		    @student = Student.create(post_params(:student, :first_name, :last_name))
+		    redirect_to student_path(@student)
+    end
+
+    def new
+        @student = Student.new
+        render 'form'
+    end
+
+    def edit
+        @student = Student.find(params[:id])
+        render 'form'
+    end
+
+    def show
+        @student = Student.find(params[:id])
+    end
+
+    def update
+        @student = Student.update(post_params(:student, :first_name, :last_name))
+		    redirect_to student_path(@student)
+    end
+
+end

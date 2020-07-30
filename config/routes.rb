76e4bf6ab14c59c1fb7ef5fa2b2fe5de24a688f1
cantstopcoding <jonathan_koss@yahoo.com>
2020-07-30 00:46:31 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :schoolclasses, only: [:index, :show, :new, :create, :edit, :update]
-  resources :students, only: [:index, :show, :new, :create, :edit, :update]
+  resources :students, except: [:destroy]
+  resources :school_classes, except: [:destroy, :index]
 end

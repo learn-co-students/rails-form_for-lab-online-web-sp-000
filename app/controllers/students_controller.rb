844cdfb33b550e365f379b2be3b09404aa9ec82c
chienleow <chienleow1@gmail.com>
2020-07-30 19:24:03 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,22 @@
+class StudentsController < ApplicationController
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.create(params.require(:student).permit(:first_name, :last_name))
+        redirect_to student_path(@student)
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+end

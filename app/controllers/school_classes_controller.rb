fe997e7f80de4c93466197868c197f104a1bdaab
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-31 18:32:47 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,27 +1,33 @@
 class SchoolClassesController < ApplicationController
 
-  def create
-    @school_class = SchoolClass.new(params.require(:school_class))
-    @school_class.save
-    redirect_to school_class_path(@school_class)
-    end
-
-  def new
-    @school_class = SchoolClass.new
-    end
+  def index
+  end
 
   def show
     @school_class = SchoolClass.find(params[:id])
-    end
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.create(school_class_params)
+    redirect_to school_class_path(@school_class)
+  end
 
   def edit
     @school_class = SchoolClass.find(params[:id])
-    end
+  end
 
   def update
     @school_class = SchoolClass.find(params[:id])
-    @school_class.update(params.require(:school_class))
+    @school_class.update(school_class_params)
     redirect_to school_class_path(@school_class)
-    end
+  end
 
+  private
+  def school_class_params
+    params.require(:school_class).permit(:title, :room_number)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: [:new, :show, :create, :update]
-     resources :schoolclasses, only: [:new, :show, :create, :update]
+  resources :students
+  resources :school_classes
 end
 
 #only??

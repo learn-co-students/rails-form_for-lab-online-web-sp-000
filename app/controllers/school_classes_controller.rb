ee100815248dfdd7a6d5a5357d652d6cfd7afbe7
Snowing222 <snowing222@gmail.com>
2020-09-29 00:17:37 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,6 +1,6 @@
 class SchoolClassesController < ApplicationController
     def new
-
+        @schoolclass = SchoolClass.new 
     end
 
     def create
@@ -14,14 +14,17 @@ class SchoolClassesController < ApplicationController
     end
 
     def show
-
+        @schoolclass = SchoolClass.find_by(id:params[:id])
     end
 
     def edit
-
+        @schoolclass = SchoolClass.find_by(id:params[:id])
     end
 
     def update
+        schoolclass = SchoolClass.find_by(id:params[:id])
+        SchoolClass.update(params.require(:school_class).permit(:title, :room_number))
+        redirect_to school_class_path(schoolclass)
         
     end
 end

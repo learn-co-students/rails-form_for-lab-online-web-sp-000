dfb13611f36c18943d71fee2c479929423d0e219
khoinguyenkc <khoinguyenkc@gmail.com>
2020-10-29 02:36:23 -0700

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,32 @@
+class SchoolClassesController < ApplicationController
+    
+    #use strong params!!!!
+
+    def new 
+        @school_class = SchoolClass.new
+    end
+
+    def create
+        #"school_class"=>{"title"=>"ancient civilizations", "room_number"=>"214"},
+        @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
+        @school_class.save
+        redirect_to school_class_path(@school_class)
+    end
+
+    def show
+        @school_class = SchoolClass.find_by(id: params[:id])
+    end
+
+    def edit
+        @school_class = SchoolClass.find_by(id: params[:id])
+    end
+
+    def update
+        @school_class = SchoolClass.find_by(id: params[:id])
+        @school_class.update(params.require(:school_class).permit(:title, :room_number))
+        @school_class.save
+        redirect_to school_class_path(@school_class)
+    end
+
+
+end

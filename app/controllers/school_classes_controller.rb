4f658e1ab10e7f4aa5ca523be5009b9a6f86715e
anthony mcmullen <amcmullen9291@gmail.com>
2021-01-29 02:38:36 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,19 +1,2 @@
 class SchoolClassesController < ApplicationController
-
-  def new 
-  end 
-
-  def create
-    @class = SchoolClass.create(:title => params[:title], :room_number => params[:room_number])
-  end 
-  
-  def show 
-  end 
-  
-  def update 
-  end 
-  
-  def edit #to render the form
-  end
-
 end

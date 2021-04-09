401435df5e4b96ec66bb366f899b6074d3d12fa5
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-04-09 15:11:15 -0500

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,33 @@
+class StudentsController < ApplicationController
+    def index
+        @students = Student.all
+      end
+    
+      def show
+        @student = Student.find(params[:id])
+      end
+    
+      def new
+        @student = Student.new
+      end
+    
+      def create
+        @student = Student.new
+        @student.first_name = params[:first_name]
+        @student.last_name = params[:last_name]
+        @student.save
+        redirect_to student_path(@student)
+      end
+    
+      def edit
+        @student = Student.find(params[:id])
+      end
+    
+      def update
+        @student = Student.find(params[:id])
+        @student.update(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+        redirect_to student_path(@student)
+        # raise params.inspect
+        # ^ this line will cause the application to pause and print out params on an error page
+      end
+end

e89cae73211cb11599d5af3f1a938a53e45a5d8a
Raven <ravenkoehler@gmail.com>
2021-05-04 09:25:00 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/students/:id/edit', to: 'students#edit', as: 'edit_db_student'
+  resources :students, :school_classes
 end
